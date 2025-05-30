7401deafeec759ed757ec9008f51d917c2d7892f
kamalmoundir <moundirkamal@gmail.com>
2025-05-30 13:48:41 +0200

    update  ex03 module9

--- module09/ex03/persons_of_interest.py
+++ module09/ex03/persons_of_interest.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+def famous_births(item):
+    sorted_items = sorted(item.items(), key=lambda kv: (kv[1]["date_of_birth"], kv[1]["name"]))
+    for n in sorted_items:
+        print(n[1]["name"] +" is a great scientist born in " +n[1]["date_of_birth"])
+
 women_scientists = {
 "ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
 "cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
